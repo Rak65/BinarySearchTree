9a03e7a5e40f6b14befe0b03299b5b4f1b8132d2
Rak65 <rakeshp6505@gmail.com>
2023-05-31 20:27:44 +0530

    Ability to create a BST by adding 56 and then adding 30 and 70

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public Node<T> root;    // Root node of the tree
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Add(T key)
+        {
+            root = AddRecursive(root, key);    // Call the recursive helper method to add a node
+        }
+
+        // Recursive helper method to add a node
+        private Node<T> AddRecursive(Node<T> current, T key)
+        {
+            if (current == null)    // If the current node is null, create a new node with the key
+                return new Node<T>(key);
+
+            // Compare the key with the current node's key and recursively add the key to the appropriate subtree
+            if (key.CompareTo(current.Key) < 0)
+                current.Left = AddRecursive(current.Left, key);
+            else if (key.CompareTo(current.Key) > 0)
+                current.Right = AddRecursive(current.Right, key);
+
+            return current;
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,7 +10,28 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Binary Search Tree");
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();    // Create an instance of the binary search tree
+
+            bst.Add(56);    // Add root node with key 56
+            bst.Add(30);    // Add node with key 30 as left child of 56
+            bst.Add(70);    // Add node with key 70 as right child of 56
+
+            // Print the tree structure for visualization
+            PrintTree(bst.root);
+
+            Console.WriteLine("BST creation completed.");
+        }
+        // Helper method to print the tree structure
+        static void PrintTree<T>(Node<T> node) where T : IComparable<T>
+        {
+            if (node == null)
+                return;
+
+            PrintTree(node.Left);    // Recursively print the left subtree
+
+            Console.WriteLine(node.Key);    // Print the key value of the node
+
+            PrintTree(node.Right);    // Recursively print the right subtree
         }
     }
 }
